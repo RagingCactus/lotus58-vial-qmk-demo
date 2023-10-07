47d2c4ceaf6809dbb710ba0fb5b51544c721609d
TweetyDaBird <markus.knutsson@tweety.se>
2023-10-07 19:28:00 +0000

    tweaked

---
+++ keyboards/tweetydabird/chameleon/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x40, 0x13, 0xA0, 0xCD, 0x14, 0x2B, 0xB5, 0xB0}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_COMBO_COUNT 2
+#define DYNAMIC_KEYMAP_MACRO_COUNT 2
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
